c8979a0156c9f2fd3d556c721a5737d55d516f9d
ADITYA SHARMA <sharmaaditya13064@gmail.com>
2022-10-17 17:41:40 +0530

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Preview from './Preview';
+import Speed from './Speed';
+import getText from './getText';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  text: getText(),
+  userInput: '',
+  symbols: 0,
+  sec: 0,
+  started: false,
+  finished: false
+}
+
+class App extends Component {
+
+  state = initialState;
+
+  onRestart = () => {
+    this.setState(initialState)
+  }
+
+  onUserInputChange = (e) => {
+    const v = e.target.value;
+    this.setTimer();
+    this.onFinish(v)
+    this.setState({
+      userInput: v,
+      symbols: this.countCorrectSymbols(v)
+    })
+  }
+
+  onFinish(userInput) {
+    if (userInput === this.state.text) {
+      clearInterval(this.interval);
+      this.setState({
+        finished: true
+      })
+    }
+  }
+
+  countCorrectSymbols(userInput) {
+    const text = this.state.text.replace(' ', '');
+    return userInput.replace(' ', '').split('').filter((s,i) => s === text[i]).length;
+  }
+
+  setTimer() {
+    if (!this.state.started) {
+      this.setState({started: true});
+      this.interval = setInterval(() => {
+        this.setState(prevProps => {
+          return {sec: prevProps.sec + 1}
+        })
+      }, 1000)
+    }
+  }
+
+  render() {
+    return (
+      <div className="container mt-5 mb-5 border bg-primary text-dark">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <Preview text={this.state.text} userInput={this.state.userInput}/>
+            <textarea
+              value={this.state.userInput}
+              onChange={this.onUserInputChange}
+              className="form-control mb-3"
+              placeholder="Start typing..."
+              readOnly={this.state.finished}
+            ></textarea>
+            <Speed sec={this.state.sec} symbols={this.state.symbols}/>
+            <div className="text-right">
+              <button className="btn btn-dark" onClick={this.onRestart}>Restart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
